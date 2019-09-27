8861d69c95c086676a2185db09243d107b54846b
Ewald van der Westhuizen <ewaldvdw@gmail.com>
2019-09-27 15:09:45 +0200

    Adding Babel recipe as a starting point for the NCHLT BNF extractor.

---
+++ egs/nchlt_multi_bnfs/s5/readme.md
@@ -0,0 +1,3 @@
+# Readme for the NCHLT BNF extractor
+
+
---
+++ egs/nchlt_multi_bnfs/s5/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# The main run script to train the NCHLT BNF extractor.
+# The recipe is based on the Babel multilingual BNF extractor.
+
+
+# Prepare the NCHLT corpora for input to Kaldi.
+
+
+
+
+
+
+# Train the BNF extractor.
+local/nnet3/run_multilingual_bnf.sh
+
