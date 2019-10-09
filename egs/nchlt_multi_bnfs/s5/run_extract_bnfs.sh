43087bffcb1acc891f48aa49fe0f09c02b4af12f
Ewald van der Westhuizen <ewaldvdw@gmail.com>
2019-10-09 08:39:08 +0200

    Added a final step to the extraction script that can convert the binary feature archives to text. This is sometimes handy for importing smaller data sets into non-Kaldi projects.

--- egs/nchlt_multi_bnfs/s5/run_extract_bnfs.sh
+++ egs/nchlt_multi_bnfs/s5/run_extract_bnfs.sh
@@ -9,6 +9,9 @@ set -o pipefail  #Exit if any of the commands in the pipeline will
                  #return non-zero return code
 
 set -u           #Fail on an undefined variable
+
+nj=10
+
 bnf_model_dir="models/nnet3/9lang"
 global_extractor="models/multi/nnet3/extractor"
 
@@ -26,7 +29,7 @@ exp_dir="exp"
 # Make hi-res MFCC features.
 mfccdir=mfcc_hires
 if [ ! -f "${mfccdir}/.done" ]; then
-    steps/make_mfcc.sh --cmd "$train_cmd" --mfcc-config conf/mfcc_hires.conf --nj 10 ${data_hires_dir} exp/make_mfcc_hires/train $mfccdir
+    steps/make_mfcc.sh --cmd "$train_cmd" --mfcc-config conf/mfcc_hires.conf --nj ${nj} ${data_hires_dir} exp/make_mfcc_hires/train $mfccdir
     utils/fix_data_dir.sh ${data_hires_dir}
     steps/compute_cmvn_stats.sh ${data_hires_dir} exp/make_mfcc/train $mfccdir
     utils/fix_data_dir.sh ${data_hires_dir}
@@ -35,7 +38,7 @@ fi
 
 mfccdir=mfcc_hires_pitch
 if [ ! -f "${mfccdir}/.done" ]; then
-    steps/make_mfcc_pitch.sh --cmd "$train_cmd" --mfcc-config conf/mfcc_hires.conf --pitch-config conf/pitch.conf --nj 10 ${data_hires_pitch_dir} exp/make_mfcc_hires_pitch/train $mfccdir
+    steps/make_mfcc_pitch.sh --cmd "$train_cmd" --mfcc-config conf/mfcc_hires.conf --pitch-config conf/pitch.conf --nj ${nj} ${data_hires_pitch_dir} exp/make_mfcc_hires_pitch/train $mfccdir
     utils/fix_data_dir.sh ${data_hires_pitch_dir}
     steps/compute_cmvn_stats.sh ${data_hires_pitch_dir} exp/make_mfcc/train $mfccdir
     utils/fix_data_dir.sh ${data_hires_pitch_dir}
@@ -46,7 +49,7 @@ fi
 
 if [ ! -f "${ivector_dir}/.done" ]; then
     steps/online/nnet2/extract_ivectors_online.sh \
-        --cmd "$train_cmd" --nj 10 \
+        --cmd "$train_cmd" --nj ${nj} \
         $data_hires_dir $global_extractor $ivector_dir || exit 1;
     touch "${ivector_dir}/.done"
 fi
@@ -56,11 +59,21 @@ fi
 [ ! -d $dump_bnf_dir ] && mkdir -p $dump_bnf_dir
 if [ ! -f $data_bnf_dir/.done ]; then
     # put the archives in ${dump_bnf_dir}/.
-    steps/nnet3/make_bottleneck_features.sh --use-gpu true --nj 10 --cmd "$train_cmd" \
+    steps/nnet3/make_bottleneck_features.sh --use-gpu true --nj ${nj} --cmd "$train_cmd" \
         --ivector-dir $ivector_dir \
         tdnn_bn.renorm $data_hires_pitch_dir $data_bnf_dir \
         $bnf_model_dir $dump_bnf_dir/log $dump_bnf_dir || exit 1;
     touch $data_bnf_dir/.done
+fi
+
+if false; then
+    # If a text version of the features are required, set false to true to run this step.
+    # Copy the feature to convert them to text format for easy inspection.
+    echo "Converting binary features to text format."
+    $train_cmd JOB=1:$nj $dump_bnf_dir/log/bin_to_txt.JOB.log \
+        copy-feats ark:$dump_bnf_dir/raw_bnfeat_train_hires_pitch.JOB.ark \
+        ark,t:$dump_bnf_dir/raw_bnfeat_train_hires_pitch.JOB.txt \
+        || exit 1;
 fi
 
 
